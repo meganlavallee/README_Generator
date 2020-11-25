d279a648c9059a5b7b8ce2906e22b3ac95b97039
Megan Lavallee <megan.lavallee@yahoo.com>
2020-11-25 12:08:48 -0700

    styling

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-const inquirer = require('inquirer');
-
+const inquirer = require("inquirer");
 
 inquirer
-    .prompt([
-        {
+  .prompt([
+    {
+      // Installation
+      type: "input",
+      message: "How do I install this project?",
+      name: "installation",
+    },
+    {
+      //Usage
+      type: "usage",
+      message: "What is the projects usage?",
+      name: "usage",
+    },
 
-            // Installation
-            type: 'input',
-            message: 'How do I install this project?',
-            name: 'installation',
-        },
-        {
+    //Credits
+    {
+      type: "communication",
+      message: "Collaborators are listed here.",
+      name: "credits",
+    },
 
-            //Usage
-            type: 'usage',
-            message: 'What is the projects usage?',
-            name: 'usage',
-        },
-
-            //Credits
-        {
-            type: 'communication',
-            message: 'Collaborators are listed here.',
-            name: 'credits',
-        },
-
-        {
-
-            // License
-            type: 'input',
-            message: 'What is the licensing agreement?',
-            name: 'license',
-        },
-        {
-
-            // Contact Info
-            type: 'input',
-            message: 'How Do I Reach You?',
-            name: 'contact',
-        },
-    ])
-    .then((response) =>
-        response.confirm === response.password
-            ? console.log('Success!')
-            : console.log('It works')
-    );
+    {
+      // License
+      type: "input",
+      message: "What is the licensing agreement?",
+      name: "license",
+    },
+    {
+      // Contact Info
+      type: "input",
+      message: "How Do I Reach You?",
+      name: "contact",
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log("Success!")
+      : console.log("It works")
+  );
