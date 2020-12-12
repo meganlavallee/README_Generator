f59e7d54cbb6ccd1be59c03fd6b4b84803182165
Megan Lavallee <megan.lavallee@yahoo.com>
2020-12-12 12:03:30 -0700

    markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,99 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.installation}
+`;
+}
 
+
+
+// function to generate markdown for README
+function generateMarkdown(answers) {
+  const {
+    title,
+    description,
+    installation,
+    contributors,
+    test,
+    license,
+    licenseUrl,
+    githubUsername,
+    email,
+  } = answers;
+
+  return `# Title: **${title}**
+
+  &nbsp;
+  &nbsp;
+  ## Description:
+
+  **${description}**
+
+  &nbsp;
+  &nbsp;
+  ## Installation: 
+
+  *${installation}*
+
+
+  &nbsp;
+  &nbsp;
+  ## Contributors: 
+  
+  **${contributors}**
+
+
+  &nbsp;
+  &nbsp;
+  ## Test: 
+  
+  **${test}**
+
+
+  &nbsp;
+  &nbsp;
+  ## License: 
+  
+  ${license}
+
+
+  &nbsp;
+  &nbsp;
+  ## License URL: 
+  ${licenseUrl}
+
+
+  &nbsp;
+  &nbsp;
+  ## GitHub UserName:
+  
+  @${githubUsername}
+
+
+  &nbsp;
+  &nbsp;
+  ## Email: 
+  
+  **${email}**
+
+  &nbsp;
+  &nbsp;
+  # Screen Shots
+  &nbsp;
+  &nbsp;
+  ![Screenshot (21)]()
+  &nbsp;
+  &nbsp;
+  ![Screenshot (22)]()
+  &nbsp;
+  &nbsp;
+  # Video Clip
+  &nbsp;
+  &nbsp;
+  ![](readme.gif)
+  &nbsp;
+  &nbsp;
+  ![](readmepreview.gif)
 `;
 }
 
 module.exports = generateMarkdown;
-
-// markdown template for each one
-// template literal
